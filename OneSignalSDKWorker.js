5e570dda582e631c288407f2b5524e25f8168e4f
Sumit Adhikari <102236671+adhikarisumit@users.noreply.github.com>
2023-11-01 18:40:35 +0900

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,15 @@
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+document.addEventListener("DOMContentLoaded", function () {
+  const inputText = document.getElementById("inputText");
+  const convertButton = document.getElementById("convertButton");
+  const outputText = document.getElementById("outputText");
+
+  convertButton.addEventListener("click", function () {
+    const kanjiText = inputText.value;
+
+    // Implement your Kanji to Furigana conversion logic here
+    // This is where you would analyze the input text and generate Furigana
+
+    // For a basic example, just display the same text for now
+    outputText.textContent = kanjiText;
+  });
+});
